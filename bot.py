e755306a73dc7efb1372114a5a1c747eec00ca66
MoriiHuang <90389782+MoriiHuang@users.noreply.github.com>
2022-06-25 23:33:54 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,8 +20,8 @@ app = Ariadne(
     broadcast=bcc,
     connect_info=MiraiSession(
         host="http://localhost:3000",  # 填入 HTTP API 服务运行的地址
-        verify_key="hcytsl",  # 填入 verifyKey
-        account=3237855048,  # 你的机器人的 qq 号
+        verify_key="",  # 填入 verifyKey
+        account=,  # 你的机器人的 qq 号
     )
 )
 from graia.scheduler import GraiaScheduler
@@ -29,7 +29,7 @@ sche = app.create(GraiaScheduler)
 from graia.scheduler import timers
 @sche.schedule(timers.crontabify('00 23 * * *'))
 async def every_minute_speaking(app: Ariadne):
-    await app.sendGroupMessage(1072621520, MessageChain.create("熬夜虽好，也不要贪玩哦"))
+    await app.sendGroupMessage(, MessageChain.create("熬夜虽好，也不要贪玩哦"))
 saya = Saya(bcc)
 saya.install_behaviours(
     BroadcastBehaviour(bcc),
